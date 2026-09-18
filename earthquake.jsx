303aa8ad4723e8fa6b87e8fcf5b53cbe3f2e8224
Ramiro Lacci <139280601+ramirolacci@users.noreply.github.com>
2026-09-18 09:28:10 -0300

    Problem resolved on the map

--- earthquake.jsx
+++ earthquake.jsx
@@ -559,8 +559,9 @@ function MapaLeaflet() {
                 zoomControl={false}
             >
                 <TileLayer
-                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                    url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+                    attribution='&copy; <a href="https://www.esri.com/">Esri</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    maxZoom={16}
                 />
                 
                 <MapRecenter center={mapCenter} zoom={mapZoom} />
